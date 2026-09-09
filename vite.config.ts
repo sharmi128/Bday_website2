e27efd1c6d307e2f81782500863dae2f74f85272
SHARMI K <211102334+sharmi128@users.noreply.github.com>
2026-09-09 21:40:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Bday_website2/',
   plugins: [react()],
+  base: '/Bday_website2/',
 })
